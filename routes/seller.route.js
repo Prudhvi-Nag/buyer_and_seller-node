22ad0a57d19b0fa736f3dd3751499c9bd34d2ec3
Prudhvi Nag <prudhvi.kammela@motivitylabs.com>
2024-05-26 19:40:07 +0530

    login and sign up endpoints upodated

--- routes/seller.route.js
+++ routes/seller.route.js
@@ -8,8 +8,8 @@ const { userAuth } = require("../middleware/middleware");
 
 const router = express.Router();
 
-router.route("/auth/seller/signup").post(sellerSignUp);
-router.route("/auth/seller/signin").post(sellerSignIn);
+router.route("/signup").post(sellerSignUp);
+router.route("/signin").post(sellerSignIn);
 router.route("/seller/details").get(userAuth, sellerDetails);
 router.route("/seller/details").patch(userAuth, sellerDetails);
 
